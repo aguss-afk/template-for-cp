5195f2f37939cc95cfe19b1b714130f94a0fa056
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2023-06-20 18:34:01 -0400

    Create main.cpp
    Added main template.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve(){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
+
