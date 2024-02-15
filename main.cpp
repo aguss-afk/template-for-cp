92b920013e715a29c110ff8b7ab8094159cc85d5
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2024-02-15 15:25:35 +0100

    new best pro template pro cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#define endl "\n"
+#define sp " "
+#define MOD 1000000007
+#define f1(i, x) for(auto &i : x)
+#define f2(i, x) for(ll i = 0; i < x; i++)
+#define raya() cout << endl << "====================================" << endl
 using namespace std;
-
 typedef long long ll;
-
 void solve(){
     
 }
-
 int main(){
     ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie();
+    cout.tie();
     int t;
     cin >> t;
     while(t--)
         solve();
 }
-
