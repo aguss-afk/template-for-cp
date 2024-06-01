4e46e61ea4956c551d6e6c68b5b9482141f05dda
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2024-06-01 11:12:44 -0400

    modified inf
    changed 
    #define inf LLONG_MAX
    to 
    #define INF LLONG_MAX

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #define f2(i, x, j) for(ll i = j; i < x; i++)
 #define raya() cout << endl << "====================================" << endl
 #define dbg(x) cerr << #x << ": " << x << endl;
-#define inf LLONG_MAX
+#define INF LLONG_MAX
 using namespace std;
 typedef long long ll;
 void solve(){
