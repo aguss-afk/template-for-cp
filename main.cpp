184b0eb59af0009375a8529a6d106748bce8b708
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2025-02-02 13:08:54 -0400

    remove trash

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
 
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-
 #define dbg(x) cerr << #x << ": " << x << '\n';
 #define dbgv(v) cerr << #v << ": "; for(auto &el : v) cerr << el << " "; cerr << '\n';
 
 using namespace std;
 using ll = long long;
 
-const ll MOD = 1e9 + 7;
-const ll INF = 1e18 + 5;
-
 void solve(){
     
 }
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
